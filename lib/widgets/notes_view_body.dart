4b8df32651fa162338da3e19c7b61ed9d7976321
MahmoudHany <mahmoud.hany.attia@gmail>
2025-02-19 20:15:28 +0200

    create note item

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/widgets/custom_app_bar.dart';
+import 'package:note_app/widgets/custom_note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -12,33 +13,11 @@ class NotesViewBody extends StatelessWidget {
         children: [
           SizedBox(height: 50),
           CustomAppBar(),
+          NoteItem(),
         ],
       ),
     );
   }
 }
 
-class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.yellow.withOpacity(0.1),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        children: [
-          ListTile(
-            title: const Text("Flutter tips"),
-
-            subtitle: const Text("Flutter tips and tricks"),
-            trailing: IconButton(onPressed: (){}, icon: const Icon(Icons.delete)), 
-          )
-        ],
-      ),
-    );
-  }
-}
 
