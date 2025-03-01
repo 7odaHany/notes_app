7e1195e82271e3b49753556b52f13a4528cafaba
MahmoudHany <mahmoud.hany.attia@gmail>
2025-03-02 01:48:58 +0200

    finish note button sheet

--- lib/widgets/add_note_buttom_sheet.dart
+++ lib/widgets/add_note_buttom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/widgets/custom_button.dart';
 import 'package:note_app/widgets/custom_text_filed.dart';
 
 class AddNoteButtonSheet extends StatelessWidget {
@@ -8,65 +9,40 @@ class AddNoteButtonSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 16,
-          ),
-           Text(
-            'Add Note',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          CustomTextFiled(
-            hintText: 'Title',
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          CustomTextFiled(
-            hintText: 'content',
-            maxLines: 5,
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 16,
+            ),
+            Text(
+              'Add Note',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextFiled(
+              hintText: 'Title',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextFiled(
+              hintText: 'content',
+              maxLines: 5,
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            CustomButton(),
+            SizedBox(
+              height: 24,
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-/*
-Container(
-      padding: const EdgeInsets.all(24),
-      child: Column(
-        children: [
-          const Text(
-            'Add Note',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 16),
-          TextField(
-            decoration: InputDecoration(
-              hintText: 'Title',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8),
-              ),
-            ),
-          ),
-          const SizedBox(height: 16),
-          const TextField(
-            maxLines: 5,
-            decoration: InputDecoration(
-              hintText: 'Description',
-              border: OutlineInputBorder(),
-            ),
-          ),
-          const SizedBox(height: 16),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Add Note'),
-          ),
-        ],
-      ),
-    );
-*/
+
