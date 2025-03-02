400f1df1e025e641617f1db80128a7b97c04597e
MahmoudHany <mahmoud.hany.attia@gmail>
2025-03-02 02:07:03 +0200

    create edit note view

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,53 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(
-        top: 24,
-        bottom: 24,
-        left: 16,
-      ),
-      decoration: BoxDecoration(
-        color: const Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              "Flutter tips",
-              style: TextStyle(color: Colors.black, fontSize: 27),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const EditNoteView()),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(
+          top: 24,
+          bottom: 24,
+          left: 16,
+        ),
+        decoration: BoxDecoration(
+          color: const Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                "Flutter tips",
+                style: TextStyle(color: Colors.black, fontSize: 27),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(
+                  top: 16,
+                  bottom: 16,
+                ),
+                child: Text(
+                  "Flutter tips and tricks , bla bla bla",
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(0.5), fontSize: 18),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.delete,
+                  size: 30,
+                ),
+                color: Colors.black,
+              ),
             ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16 , bottom: 16,),
+            Padding(
+              padding: const EdgeInsets.only(right: 16),
               child: Text(
-                "Flutter tips and tricks , bla bla bla",
-                style:
-                    TextStyle(color: Colors.black.withOpacity(0.5), fontSize: 18),
+                "feb21 , 2025",
+                style: TextStyle(
+                    color: Colors.black.withOpacity(.5), fontSize: 14),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.delete,
-                size: 30,
-              ),
-              color: Colors.black,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 16),
-            child: Text(
-              "feb21 , 2025",
-              style: TextStyle(color: Colors.black.withOpacity(.5) , fontSize: 14),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
