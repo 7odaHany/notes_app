cf40ae4db13def05067007c86f33938b335595e1
MahmoudHany <mahmoud.hany.attia@gmail>
2025-03-07 03:59:15 +0200

    add hive

--- lib/widgets/edit_view_body.dart
+++ lib/widgets/edit_view_body.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/widgets/custom_app_bar.dart';
+import 'package:note_app/widgets/custom_text_filed.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          CustomAppBar(
+            title: "Edit Note",
+            icon: Icons.check,
+          ),
+          SizedBox(
+            height: 32,
+          ),
+          CustomTextFiled(hintText: "Title"),
+          SizedBox(
+            height: 16,
+          ),
+          CustomTextFiled(
+            hintText: "Content",
+            maxLines: 5,
+          )
+        ],
+      ),
+    );
   }
 }
