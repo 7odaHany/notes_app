31af9628c95551031b500433212f4ea745afcbdf
MahmoudHany <mahmoud.hany.attia@gmail>
2025-03-07 05:41:24 +0200

    take input & validate data

--- lib/widgets/add_note_buttom_sheet.dart
+++ lib/widgets/add_note_buttom_sheet.dart
@@ -10,37 +10,76 @@ class AddNoteButtonSheet extends StatelessWidget {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 16,
-            ),
-            Text(
-              'Add Note',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            CustomTextFiled(
-              hintText: 'Title',
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            CustomTextFiled(
-              hintText: 'content',
-              maxLines: 5,
-            ),
-            SizedBox(
-              height: 32,
-            ),
-            CustomButton(),
-            SizedBox(
-              height: 24,
-            ),
-          ],
-        ),
+        child: AddNoteForm(),
+      ),
+    );
+  }
+}
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String ? title , subTitle;
+  @override
+  Widget build(BuildContext context) {
+    return  Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child:  Column(
+        children: [
+          const SizedBox(
+            height: 16,
+          ),
+          const Text(
+            'Add Note',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          CustomTextFiled(
+            hintText: 'Title',
+            onSaved: (value){
+              title = value;
+            },
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          CustomTextFiled(
+            hintText: 'content',
+            maxLines: 5,
+            onSaved: (value){
+              subTitle = value;
+            },
+          ),
+          const SizedBox(
+            height: 32,
+          ),
+           CustomButton(
+            onTap: (){
+              if(formKey.currentState!.validate()){
+                formKey.currentState!.save();
+              }else {
+                setState(() {
+                  autovalidateMode = AutovalidateMode.always;
+                });
+              }
+            },
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+        ],
       ),
     );
   }
--- lib/widgets/custom_text_filed.dart
+++ lib/widgets/custom_text_filed.dart
@@ -2,12 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:note_app/constants.dart';
 
 class CustomTextFiled extends StatelessWidget {
-  const CustomTextFiled({super.key, required this.hintText,  this.maxLines = 1});
-final String hintText;
-final int maxLines;
+  const CustomTextFiled(
+      {super.key, required this.hintText, this.maxLines = 1, this.onSaved});
+  final String hintText;
+  final int maxLines;
+  final void Function(String?)? onSaved;
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      onSaved: onSaved,
+      validator: (value) {
+        if (value?.isEmpty ?? true) { // if value is empty or null
+          return 'Please enter $hintText';
+        }else{
+          return null;
+        }
+      },
       cursorColor: kPrimaryColor,
       maxLines: maxLines,
       decoration: InputDecoration(
@@ -20,11 +30,11 @@ final int maxLines;
     );
   }
 
-  OutlineInputBorder buildBorder([ color]) {
+  OutlineInputBorder buildBorder([color]) {
     return OutlineInputBorder(
         borderRadius: BorderRadius.circular(8),
         borderSide: BorderSide(
-          color: color ??Colors.white,
+          color: color ?? Colors.white,
         ));
   }
 }
